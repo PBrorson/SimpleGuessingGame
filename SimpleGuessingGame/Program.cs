777056d93daf4bde6c0eba72c99298bb7a11108f
Patrik <patrikbrorson@hotmail.com>
2021-11-20 12:24:12 +0100

    Add project files.

---
+++ SimpleGuessingGame/Program.cs
@@ -0,0 +1,28 @@
+﻿string secretWord = "monkey";
+string guess = "";
+int guessCount = 0;
+int guessLimit = 3;
+bool outOfGuesses = false;
+
+while (guess != secretWord && !outOfGuesses)
+{
+    if (guessCount < guessLimit)
+    {
+        Console.WriteLine("Enter a guess! ");
+        guess = Console.ReadLine();
+        guessCount++;
+    }
+    else
+    {
+        outOfGuesses = true;
+    }
+}
+
+if (outOfGuesses)
+{
+    Console.WriteLine("Sorry,you lose!");
+}
+else
+{
+    Console.WriteLine("Congrats,you win!");
+}
